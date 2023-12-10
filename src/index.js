a9d93e62c826202897d8f61fa4366a392d78b16d
ToporekDaniel <danieltoporek0@gmail.com>
2023-12-10 21:07:38 +0100

    powoli

--- src/index.js
+++ src/index.js
@@ -11,7 +11,34 @@ axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 function fetchBreeds() {
   return axios.get('/breeds').then(res => {
-    return console.log(res.data);
+    return res.data;
   });
 }
-fetchBreeds();
+
+fetchBreeds().then(data => {
+  data.map(breed => {
+    const selectItem = document.createElement('option');
+    selectItem.value = breed.id;
+    selectItem.text = breed.name;
+    selector.insertAdjacentElement('beforeend', selectItem);
+  });
+});
+
+fetchBreeds().then(data => {
+  console.log(data[0]);
+});
+
+fetchBreeds().then(data => {
+  data.map(breed => {
+    console.log(breed.id);
+    console.log(breed.name);
+    console.log(breed.description);
+    console.log(breed.image.url);
+    const image = document.createElement('img');
+    image.src = breed.image.url;
+    image.alt = 'This is a image of a cat';
+    image.width = 300;
+    image.height = 200;
+    info.insertAdjacentElement('beforeend', image);
+  });
+});
