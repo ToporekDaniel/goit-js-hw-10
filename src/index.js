c4edb85afa49700cff879c195d18d9cc2c841cf7
ToporekDaniel <danieltoporek0@gmail.com>
2023-12-14 19:21:47 +0100

    pełna funkcjonalność

--- src/index.js
+++ src/index.js
@@ -10,43 +10,91 @@ axios.defaults.headers.common['x-api-key'] =
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 function fetchBreeds() {
-  return axios.get('/breeds').then(res => {
-    return res.data;
-  });
+  return axios
+    .get('/breeds')
+    .then(res => {
+      return res.data;
+    })
+    .catch(error => {
+      showError();
+    });
 }
 
-fetchBreeds().then(data => {
-  const initialOption = document.createElement('option');
-  initialOption.value = 0;
-  initialOption.text = 'Wybierz kota';
-  selector.appendChild(initialOption);
+function showError() {
+  errorMsg.style.display = 'block';
+}
+function hideError() {
+  errorMsg.style.display = 'none';
+}
+function showLoading() {
+  loadingTxt.style.display = 'block';
+}
+function hideLoading() {
+  loadingTxt.style.display = 'none';
+}
+hideError();
+fetchBreeds()
+  .then(data => {
+    hideLoading();
+    const initialOption = document.createElement('option');
+    initialOption.value = 0;
+    initialOption.text = 'Wybierz kota';
+    selector.appendChild(initialOption);
 
-  data.map(breed => {
-    const selectItem = document.createElement('option');
-    selectItem.value = breed.id;
-    selectItem.text = breed.name;
-    selector.insertAdjacentElement('beforeend', selectItem);
-  });
-});
+    data.map(breed => {
+      const selectItem = document.createElement('option');
+      selectItem.value = breed.id;
+      selectItem.text = breed.name;
+      selector.insertAdjacentElement('beforeend', selectItem);
+    });
+  })
+  .catch(showError);
+
+// za wiele kombinowania
+
+// function findCats(catID, thing, catData) {
+//   const foundCat = catData.find(breed => breed.id === catID);
+//   if (foundCat) {
+//     return foundCat[thing];
+//   }
+//   return '';
+// }
 
 selector.addEventListener('change', event => {
+  showLoading();
+  hideError();
+  info.innerHTML = '';
   const catID = event.target.value;
   // console.log(catID);
 
-  fetchBreeds().then(data => {
-    // console.log(data.find(breed => breed.id === catID).description);
-    info.innerHTML = '';
-    const image = document.createElement('img');
-    image.src = data.find(breed => breed.id === catID).image.url;
-    image.alt = 'This is a image of a cat';
-    image.width = 500;
-    image.height = 350;
-    info.insertAdjacentElement('beforeend', image);
-    info.insertAdjacentHTML(
-      'beforeend',
-      '<br></br>' + data.find(breed => breed.id === catID).description
-    );
-  });
+  fetchBreeds()
+    .then(data => {
+      // console.log(data.find(breed => breed.id === catID));
+
+      // tak jest zdecydowanie prościej
+      const selectedCat = data.find(breed => breed.id === catID);
+
+      const image = document.createElement('img');
+      image.src = data.find(breed => breed.id === catID).image.url;
+      image.alt = 'This is a image of a cat';
+      image.height = 500;
+      info.insertAdjacentElement('beforeend', image);
+      info.insertAdjacentHTML('beforeend', `<h2>${selectedCat.name}</h2>`);
+      info.insertAdjacentHTML(
+        'beforeend',
+        `<p><b>Description:</b> ${selectedCat.description}</p>`
+      );
+      info.insertAdjacentHTML(
+        'beforeend',
+        `<p><b>Temperament:</b> ${selectedCat.temperament}</p>`
+      );
+      hideLoading();
+    })
+    .catch(error => {
+      hideLoading();
+      showError();
+      console.error(error);
+    });
 });
 
 // fetchBreeds().then(data => {
